59e6ad671e74cc4b1da1902ede270d862a5ec69d
hakan4vg <66286478+hakan4vg@users.noreply.github.com>
2023-09-18 22:12:17 +0300

    Fixed account creation for GuessingGame

--- GuessingGame/GuessingGame.java
+++ GuessingGame/GuessingGame.java
@@ -17,70 +17,69 @@ public class GuessingGame {
             if (!user.getPassword().equals(password)) {
                 System.out.println("Incorrect password");
                 return;
-        }
-        if (user.getCoinBalance() < betAmount) {
-            System.out.println("Insufficient balance");
-            return;
-        }
-        user.setCoinBalance(user.getCoinBalance()-betAmount);
-        double newBalance;
-        int lotteryNumber = (int)(Math.random()*100);
+            }
+            if (user.getCoinBalance() < betAmount) {
+                System.out.println("Insufficient balance");
+                return;
+            }
+            user.setCoinBalance(user.getCoinBalance()-betAmount);
+            double newBalance;
+            int lotteryNumber = (int)(Math.random()*100);
 
-        int lotteryNumber1= lotteryNumber/10;
-        int lotteryNumber2= lotteryNumber%10;
+            int lotteryNumber1= lotteryNumber/10;
+            int lotteryNumber2= lotteryNumber%10;
 
-        int guessNumber1=guessedNumber/10;
-        int guessNumber2=guessedNumber%10;
+            int guessNumber1=guessedNumber/10;
+            int guessNumber2=guessedNumber%10;
 
-        System.out.println("The lottery number is "+lotteryNumber);
-        System.out.flush();
+            System.out.println("The lottery number is "+lotteryNumber);
+            System.out.flush();
 
 //        Compares the guess with lottery number.
-        if(guessedNumber==lotteryNumber){
-            System.out.println("You've guessed it exactly. Congratulations");
-            System.out.println("Your earnings are $"+(betAmount*10));
-            System.out.flush();
-            newBalance = user.getCoinBalance()+(betAmount*10);
-            user.setCoinBalance(newBalance);
-            System.out.println("Your new balance is "+user.getCoinBalance());
-            System.out.flush();
+            if(guessedNumber==lotteryNumber){
+                System.out.println("You've guessed it exactly. Congratulations");
+                System.out.println("Your earnings are $"+(betAmount*10));
+                System.out.flush();
+                newBalance = user.getCoinBalance()+(betAmount*10);
+                user.setCoinBalance(newBalance);
+                System.out.println("Your new balance is "+user.getCoinBalance());
+                System.out.flush();
+            }
+            else if ((guessNumber1==lotteryNumber2) && (guessNumber2==lotteryNumber1)){
+                System.out.println("You've guessed the numbers correctly but in the wrong order.");
+                System.out.println("Your earnings are $"+(betAmount*3.3));
+                System.out.flush();
+                newBalance = user.getCoinBalance()+(betAmount*3.3);
+                user.setCoinBalance(newBalance);
+                System.out.println("Your new balance is "+user.getCoinBalance());
+                System.out.flush();
+            }
+            else if (((guessNumber1==lotteryNumber1)&&(guessNumber2!=lotteryNumber2))||
+                    ((guessNumber2==lotteryNumber2)&&(guessNumber1)!=(lotteryNumber1))){
+                System.out.println("You've guessed a number in the correct order.");
+                System.out.println("Your earnings are $"+(betAmount*2));
+                System.out.flush();
+                newBalance = user.getCoinBalance()+(betAmount*2);
+                user.setCoinBalance(newBalance);
+                System.out.println("Your new balance is "+user.getCoinBalance());
+                System.out.flush();
+            }
+            else if ((guessNumber1==lotteryNumber2)||
+                    (guessNumber2==lotteryNumber1)){
+                System.out.println("You've guessed a number but in the wrong order. ");
+                System.out.println("Your earnings are $"+(betAmount*0.75));
+                System.out.flush();
+                newBalance = user.getCoinBalance()+(betAmount*0.75);
+                user.setCoinBalance(newBalance);
+                System.out.println("Your new balance is "+user.getCoinBalance());
+                System.out.flush();
+            }
+            else {
+                System.out.println("Your guess was wrong. Try again.");
+                System.out.println("Your new balance is " + user.getCoinBalance());
+                System.out.flush();
+            }
+            user.saveUser(username+".dat");
         }
-        else if ((guessNumber1==lotteryNumber2) && (guessNumber2==lotteryNumber1)){
-            System.out.println("You've guessed the numbers correctly but in the wrong order.");
-            System.out.println("Your earnings are $"+(betAmount*3.3));
-            System.out.flush();
-            newBalance = user.getCoinBalance()+(betAmount*3.3);
-            user.setCoinBalance(newBalance);
-            System.out.println("Your new balance is "+user.getCoinBalance());
-            System.out.flush();
-        }
-        else if (((guessNumber1==lotteryNumber1)&&(guessNumber2!=lotteryNumber2))||
-                ((guessNumber2==lotteryNumber2)&&(guessNumber1)!=(lotteryNumber1))){
-            System.out.println("You've guessed a number in the correct order.");
-            System.out.println("Your earnings are $"+(betAmount*2));
-            System.out.flush();
-            newBalance = user.getCoinBalance()+(betAmount*2);
-            user.setCoinBalance(newBalance);
-            System.out.println("Your new balance is "+user.getCoinBalance());
-            System.out.flush();
-        }
-        else if ((guessNumber1==lotteryNumber2)||
-                (guessNumber2==lotteryNumber1)){
-            System.out.println("You've guessed a number but in the wrong order. ");
-            System.out.println("Your earnings are $"+(betAmount*0.75));
-            System.out.flush();
-            newBalance = user.getCoinBalance()+(betAmount*0.75);
-            user.setCoinBalance(newBalance);
-            System.out.println("Your new balance is "+user.getCoinBalance());
-            System.out.flush();
-        }
-        else {
-            System.out.println("Your guess was wrong. Try again.");
-            System.out.println("Your new balance is " + user.getCoinBalance());
-            System.out.flush();
-        }
-        user.saveUser(username+".dat");
     }
 }
-}
-
--- test.java
+++ test.java
@@ -4,16 +4,13 @@ import GuessingGame.*;
 public class test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int selector;
-
+        int selector = 0;
         BMICalculator bmiCalculator = new BMICalculator();
         TaxCalculator taxCalculator = new TaxCalculator();
         LeapYear leapYear = new LeapYear();
         LotteryGame lotteryGame = new LotteryGame();
 
-        while (true) {
-            selector = 0; // Reset selector at the beginning of each iteration
-
+        while (true) { // Infinite loop
             // Asks the user to select which code they want to try.
             while (selector <= 0 || selector > 5) {
                 System.out.print("""
@@ -22,18 +19,21 @@ public class test {
                         3-Leap Year Calculator
                         4-Lottery Game
                         5-Lottery Game With Account Selection(WIP)
-                        -1 to exit
+                        Type 'q' to exit
                         Select which code you want to try:""");
-                selector = input.nextInt();
+                String choice = input.next();
 
-                if (selector == -1) {
-                    break; // Exit the inner loop if -1 is entered
+                if (choice.equals("q")) {
+                    System.out.println("Exiting the program.");
+                    return; // Exit the program
                 }
-            }
 
-            if (selector == -1) {
-                System.out.println("Exiting the program.");
-                break; // Exit the outer loop if -1 is entered
+                try {
+                    selector = Integer.parseInt(choice);
+                } catch (NumberFormatException e) {
+                    // Handle invalid input
+                    System.out.println("Invalid input. Please enter a valid option or 'q' to exit.");
+                }
             }
 
             System.out.println();
@@ -59,22 +59,29 @@ public class test {
                     UserDatabase userDatabase = new UserDatabase();
                     User user = userDatabase.getUserByUsername(username);
 
-                    if (user != null && user.getPassword().equals(password)) {
-                        System.out.println("Welcome, " + username + "! Your account balance is: $" + user.getCoinBalance());
-                        System.out.print("Enter your bet: ");
-                        double bet = input.nextDouble();
-                        System.out.print("Enter your guess: ");
-                        int guess = input.nextInt();
-
-                        GuessingGame guessingGame = new GuessingGame(userDatabase);
-                        guessingGame.playGame(username, password, bet, guess);
-                    } else {
-                        System.out.println("Invalid username or password. Please try again.");
+                    if (user == null) {
+                        // If the user is not found, create a new user
+                        System.out.println("Creating a new account for " + username);
+                        user = new User(username, password);
+                        userDatabase.addUser(user);
+                    } else if (!user.getPassword().equals(password)) {
+                        System.out.println("Invalid password. Please try again.");
+                        break;
                     }
+
+                    System.out.println("Welcome, " + username + "! Your account balance is: $" + user.getCoinBalance());
+                    System.out.print("Enter your bet: ");
+                    double bet = input.nextDouble();
+                    System.out.print("Enter your guess: ");
+                    int guess = input.nextInt();
+
+                    GuessingGame guessingGame = new GuessingGame(userDatabase);
+                    guessingGame.playGame(username, password, bet, guess);
                     break;
                 default:
                     System.out.println("Invalid selection. Please try again.");
             }
+            selector = 0; // Reset selector to allow the user to select again
         }
     }
 }
